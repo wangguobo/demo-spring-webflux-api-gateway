ebef994dd41ad2f03a8d92af7591116ec7cdf085
henry <503309797@qq.com>
2018-02-05 12:10:13 +0800

    modify AppInfo class

--- src/main/java/com/demo/http/api/gateway/dao/mapper/AppInfoMapper.java
+++ src/main/java/com/demo/http/api/gateway/dao/mapper/AppInfoMapper.java
@@ -3,7 +3,6 @@ package com.demo.http.api.gateway.dao.mapper;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
-
 import com.demo.http.api.gateway.model.AppInfo;
 /**
 * 接入网关的应用信息mapper
@@ -12,7 +11,7 @@ import com.demo.http.api.gateway.model.AppInfo;
 */
 @Mapper
 public interface AppInfoMapper {
-    @Select("SELECT app_key appKey,app_token token FROM thirdparty_app_info WHERE app_key = #{appKey}")
+    @Select("SELECT app_key appKey,app_secret appSecret FROM thirdparty_app_info WHERE app_key = #{appKey}")
     AppInfo findByAppKey(@Param("appKey") String appKey);
 
 }
